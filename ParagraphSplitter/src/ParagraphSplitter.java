5260f860994cd552da1177d223c4ba47f8d3f1e7
DESKTOP-HU36OR8\Art <artkrub7@gmail.com>
2016-11-15 14:56:16 +0700

    Draft 9 by tiranee sensei

--- ParagraphSplitter/src/ParagraphSplitter.java
+++ ParagraphSplitter/src/ParagraphSplitter.java
@@ -1,16 +1,46 @@
 import java.io.*;
 import java.sql.*;
+import java.util.Properties;
 
 public class ParagraphSplitter {
+	
+	private static final String CONNECTION_URL_PROPERTY = "connection.url";
+	private static final String JDBC_DRIVER_NAME_PROPERTY = "jdbc.driver.class.name";
+
+	private static String connectionUrl;
+	private static String jdbcDriverName;
+
+        private static void loadConfiguration() throws IOException {
+                InputStream input = null;
+                try {
+                        String filename = ParagraphSplitter.class.getSimpleName() + ".conf";
+                        input = ParagraphSplitter.class.getClassLoader().getResourceAsStream(filename);
+                        Properties prop = new Properties();
+                        prop.load(input);
+        
+                        connectionUrl = prop.getProperty(CONNECTION_URL_PROPERTY);
+                        jdbcDriverName = prop.getProperty(JDBC_DRIVER_NAME_PROPERTY);
+                } finally {
+                        try {
+                                if (input != null)
+                                        input.close();
+                        } catch (IOException e) {
+                                // nothing to do
+                        }
+                }
+        }
+
 	public static void main(String[] args) throws FileNotFoundException, IOException, UnsupportedEncodingException, SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+		
 		File fileDir = new File("D:\\Course 4th year\\Senior project\\AL-K-Fon.txt");
 		BufferedReader in = new BufferedReader(
 		new InputStreamReader(new FileInputStream(fileDir), "UTF8"));
 		String str;
+		loadConfiguration();
 		int i = 1;
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
 		StringBuilder paragraph = new StringBuilder();
-		//Connection conn = DriverManager.getConnection("jdbc:mysql://10.35.23.230/testjdbc?user=tagvisor&password=123456");
+		Connection conn = DriverManager.getConnection(connectionUrl);
 	//	Statement st = conn.createStatement();
 		while ((str = in.readLine()) != null) {
 			if(!str.trim().isEmpty()){
@@ -40,6 +70,22 @@ public class ParagraphSplitter {
 			}
 		}
 		in.close();
+		
+		Class.forName(jdbcDriverName);
+
+		Connection con = DriverManager.getConnection(connectionUrl);
+
+		Statement stmt = con.createStatement();
+
+		ResultSet rs = stmt.executeQuery("SELECT * FROM testStoreText");
+
+		System.out.println("\n== Begin Query Results ======================");
+
+		// print the results to the console
+		while (rs.next()) {
+			// the example query returns one String column
+			System.out.println(rs.getString(1));
+		}
 	/*	ResultSet rs = st.executeQuery("SELECT * FROM testStoreText");
 	    if (st.execute("SELECT * FROM testStoreText")) {
 	        rs = st.getResultSet();
