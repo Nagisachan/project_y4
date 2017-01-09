ad2007526e49e9acdff5cc390a5cff98d9c038e5
punyapat <perth.s28@gmail.com>
2017-01-09 12:54:09 +0700

    spicydog word segmentation

--- model/main_model.py
+++ model/main_model.py
@@ -7,41 +7,22 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import SGDClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn import metrics
-from word_segment_spicydog import Tws
+from word_segmentation_spicydog import Tws
 
-#fetch raw data
+# fetch raw data
 categories = ['alt.atheism', 'soc.religion.christian','comp.graphics', 'sci.med']
 twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
-
-# create pipeline
-text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB()),])
-
-# train model in signle line
-text_clf = text_clf.fit(twenty_train.data, twenty_train.target)
 
 # fetch test data
 twenty_test = fetch_20newsgroups(subset='test', categories=categories, shuffle=True, random_state=42)
 docs_test = twenty_test.data
-predicted = text_clf.predict(docs_test)
 
-# score
-score = np.mean(predicted == twenty_test.target)
-print "MultinomialNB",score
-
-# using different model (SVM)
+# SVM
 text_clf = Pipeline([('vect', CountVectorizer()),('tfidf', TfidfTransformer()),('clf', SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter=5, random_state=42)),])
 text_clf=text_clf.fit(twenty_train.data, twenty_train.target)
 predicted = text_clf.predict(docs_test)
 score = np.mean(predicted == twenty_test.target)
 print "SVM",score
-
-
-# using different model (multi-layer perceptron)
-text_clf = Pipeline([('vect', CountVectorizer()),('tfidf', TfidfTransformer()),('clf', MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)),])
-text_clf=text_clf.fit(twenty_train.data, twenty_train.target)
-predicted = text_clf.predict(docs_test)
-score = np.mean(predicted == twenty_test.target)
-print "NN",score
 
 # view more info
 print(metrics.classification_report(twenty_test.target, predicted, target_names=twenty_test.target_names))
