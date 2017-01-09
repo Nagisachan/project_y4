d5b3bbcb2e66f82ef3914b4c766601a6d09f88d6
punyapat <perth.s28@gmail.com>
2017-01-10 02:45:18 +0700

    update model

--- model/main_model.py
+++ model/main_model.py
@@ -8,22 +8,62 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn import metrics
 from word_segmentation_spicydog import Tws
+from raw_data_fetcher import RawData
+import string
 
+"""
 # fetch raw data
 categories = ['alt.atheism', 'soc.religion.christian','comp.graphics', 'sci.med']
+
+# fetch train data
 twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
+#print "===== Text =====\n",twenty_train.data[0]
+#print "===== Class =====\n",twenty_train.target[0]
 
 # fetch test data
 twenty_test = fetch_20newsgroups(subset='test', categories=categories, shuffle=True, random_state=42)
-docs_test = twenty_test.data
+#print "===== Text =====\n",twenty_test.data[0]
+#print "===== Class =====\n",twenty_test.target[0]
+"""
+
+raw = RawData()
+raw.load()
+
+text,tag = raw.get_train_data()
+twenty_train_data = text
+twenty_train_target = tag
+print "===== train ====="
+#for text,tag in zip(twenty_train_data,twenty_train_target):
+#	print text,tag
+
+text,tag = raw.get_test_data()
+twenty_test_data = text
+twenty_test_target = tag
+print "===== test ====="
+#for text,tag in zip(twenty_test_data,twenty_test_target):
+#	print text,tag
+	
+# prepare Custom Count Vectorizer
+def custom_preprocessor(str):
+	str = str.translate({ord(char): None for char in string.punctuation})
+	return str
+	
+def custom_tokenizer(str):
+	return str.split(' ')
+	
+count_vect = CountVectorizer(tokenizer=custom_tokenizer,analyzer = 'word',preprocessor=custom_preprocessor)
+
+# show info
+print "train sample =",len(twenty_train_target)
+print "test sample =",len(twenty_test_target)
 
 # SVM
-text_clf = Pipeline([('vect', CountVectorizer()),('tfidf', TfidfTransformer()),('clf', SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter=5, random_state=42)),])
-text_clf=text_clf.fit(twenty_train.data, twenty_train.target)
-predicted = text_clf.predict(docs_test)
-score = np.mean(predicted == twenty_test.target)
+text_clf = Pipeline([('vect', count_vect),('tfidf', TfidfTransformer()),('clf', SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter=5, random_state=42)),])
+text_clf=text_clf.fit(twenty_train_data, twenty_train_target)
+predicted = text_clf.predict(twenty_test_data)
+score = np.mean(predicted == twenty_test_target)
 print "SVM",score
 
 # view more info
-print(metrics.classification_report(twenty_test.target, predicted, target_names=twenty_test.target_names))
-print metrics.confusion_matrix(twenty_test.target, predicted)
+print(metrics.classification_report(twenty_test_target, predicted, target_names=raw.get_target_names()))
+print metrics.confusion_matrix(twenty_test_target, predicted)
---
+++ model/raw_data_fetcher.py
@@ -0,0 +1,105 @@
+from word_segmentation_spicydog import Tws
+from sklearn.feature_extraction.text import CountVectorizer
+from read_training_data import N
+import numpy as np
+import sys
+import string
+
+class RawData(object):
+	def __init__(self):
+		self.read = N()
+		self.tws = Tws()
+		self.train_text_ratio = 0.8
+		self.tag_table = {}
+		self.tag_inverse_table = {}
+		
+	def load(self):
+		text = []
+		tag = []
+
+		# read raw data
+		text,tag = self.read.read_text_tag()
+
+		# FIXME
+		# only use first 5 docs
+		text = text[:100]
+		tag = tag[:100]
+		print "[RawData]: fetch from %d docs" % len(tag)
+		# create 1 tag per doc raw data
+		new_text = []
+		new_tag = []
+		for i in range(0,len(text)):
+			tmp_text = self.tws.word_segment(text[i].strip())
+			tmp_text = " ".join([l for l in tmp_text])
+			
+			tmp_tag = tag[i].split(',')
+			tmp_tag = [t.strip() for t in tmp_tag]
+			
+			for t in tmp_tag:
+			
+				# convert tag to int
+				if t not in self.tag_table:
+					idx = len(self.tag_table)
+					self.tag_table[t] = idx
+					self.tag_inverse_table[idx] = t
+			
+				new_text.append(tmp_text)
+				new_tag.append(t)
+
+		# random sample
+		text = []
+		tag = []
+		m = len(new_tag)
+		rand_index = np.random.choice(range(1,m),m)
+		for idx in rand_index:
+			text.append(new_text[idx])
+			tag.append(self.tag_table[new_tag[idx]])
+				
+		self.text = text
+		self.tag = tag
+	
+	def get_train_data(self):
+		train_data_count = int(len(self.tag)*self.train_text_ratio)
+		return self.text[:train_data_count],self.tag[:train_data_count]
+		
+	def get_test_data(self):
+		train_data_count = int(len(self.tag)*self.train_text_ratio)
+		return self.text[train_data_count:],self.tag[train_data_count:]
+	
+	def get_target_names(self):
+		tmp = []
+		for i in range(0,len(self.tag_inverse_table)):
+			tmp.append(self.tag_inverse_table[i])
+		return tmp
+		
+if __name__ == '__main__':		
+	raw = RawData()
+	raw.load()
+	text,tag = raw.get_train_data()
+	
+	def custom_preprocessor(str):
+		str = str.translate({ord(char): None for char in string.punctuation})
+		return str
+		
+	def custom_tokenizer(str):
+		return str.split(' ')
+		
+	count_vect = CountVectorizer(tokenizer=custom_tokenizer,analyzer = 'word',preprocessor=custom_preprocessor)
+
+	for i in range(0,len(text)):
+		print "TEXT = " , text[i]
+		print "TAG = " , tag[i]	
+	print "text count =",len(text),"tag count =",len(tag)
+	
+	X_train_counts = count_vect.fit_transform(text)
+	#print(type(X_train_counts))
+	#print(X_train_counts)
+
+	index = 0
+	for word in count_vect.get_feature_names():
+		#print index, word
+		index += 1
+
+	#print raw.tag_table
+	#print raw.tag_inverse_table
+	print raw.get_target_names()
