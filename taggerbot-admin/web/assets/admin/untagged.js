9276ffe4548be9471322139267e5bfd0c5c0394a
Punyapat Sessomboon <perth.s28@gmail.com>
2017-03-29 03:25:21 +0700

    add define tag page

--- taggerbot-admin/web/assets/admin/untagged.js
+++ taggerbot-admin/web/assets/admin/untagged.js
@@ -6,20 +6,28 @@ $.ajax(SERVICE_URL, {
         for (var i = 0; i < data.data.length; i++) {
             $untagged.append(htmlFromDoc(data.data[i], i));
         }
+        $('.ui.dropdown').dropdown();
     })
 
 function htmlFromDoc(doc, i) {
     html = `<div class="item" title="` + doc.content + `">
                 <div class="right floated content">
-                    <div class="ui button blue tiny">Auto</div>
-                    <a href="` + FILE_URL.replace('FILEID', doc.file_id) + `"><div class="ui button gray tiny">Tag</div></a>
-                    <div class="ui button red tiny">Remove</div>
+                    <div class="ui teal buttons tiny">
+                        <div class="ui button">Auto</div>
+                        <div class="ui floating dropdown icon button">
+                            <i class="dropdown icon"></i>
+                            <div class="menu">
+                                <div class="item"><a href="` + FILE_URL.replace('FILEID', doc.file_id) + `"><i class="edit icon green"></i> Tag</a></div>
+                                <div class="item"><a><i class="delete icon red"></i> Remove</a></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="content">
                     <span>` + (i + 1) + `</span>
-                    <div class="ui label large">
+                    <a href="` + FILE_URL.replace('FILEID', doc.file_id) + `"><div class="ui label large">
                         ` + doc.name + `
-                    </div>
+                    </div></a>
                 </div>
             </div>`;
     return html;
